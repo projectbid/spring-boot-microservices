c1b47eb372b71fff0465134b4faebdf8c8978ac2
tabudev <tabudev@gmail.com>
2016-05-25 23:05:00 -0500

    Edited .gitignore and updated configserver

--- docker-image-all-projects.sh
+++ docker-image-all-projects.sh
@@ -2,7 +2,7 @@
 
 cd api-gateway; ./gradlew clean build distDocker; cd ..
 cd auth-server; ./gradlew clean build distDocker; cd ..
-cd config-server; ./gradlew clean build distDocker; cd ..
+cd config-server; ./gradlew clean build dockerBuildImage; cd ..
 cd task-webservice; ./gradlew clean build distDocker; cd ..
 cd user-webservice; ./gradlew clean build distDocker; cd ..
 cd web-portal; ./gradlew clean build distDocker; cd ..
